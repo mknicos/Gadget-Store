5705cec5d6faa9f9aeac411f08cf9af6256eb8b1
Matt Knicos <mknicos@gmail.com>
2014-02-15 13:23:37 -0600

    break

--- client/app/js/source/main.js
+++ client/app/js/source/main.js
@@ -18,36 +18,40 @@
   }
 
   function createUser(event){
-    debugger;
+  //Called when createUser button is clicked
     var name = $('#userName').val();
     var deposit = $('#deposit').val();
+
     var obj = {name:name, deposit: deposit};
     var url = window.location.origin.replace(/3000/, '4000') + '/users';
     var type = 'POST';
     var success = postSuccess;
 
-
     $.ajax({url: url, type: type, data:obj, success: success});
     event.preventDefault();
   }
 
+
   function postSuccess(response){
-    debugger;
+  //Takes in an object from server response, adds to user Array, and calls putUSerOnScreen function
     console.log('Post Successful');
     console.log(response[0]);
     putUserOnScreen(response[0]);
     userArray.push(response[0]);
   }
 
+
   function putUserOnScreen(user){
+  // Will take in one user object and post to user table with a buy button in frst collumn
     console.log(user);
     debugger;
-    var $userName = $('<td>').addClass('userName');
-    var $userBalance = $('<td>').addClass('userrBalance');
-    var $userItems = $('<td>').addClass('userItems');
-    var $row = $('<tr>');
-    console.log('ignore');
-    console.log($userName, $userBalance, $userItems, $row);
+    var $userName = $('<td>').text(user.name).addClass('userName');
+    var $userBalance = $('<td>').text(user.cash).addClass('userrBalance');
+    var $userItems = $('<td>').text('No Items Bought').addClass('userItems');
+    var $row = $('<tr>').data('id', user._id);
+// reuse for gadget section  var $button = $('<button>').addClass('tiny round buy');
+    $row.append($userName, $userBalance, $userItems);
+    $('#userTable').append($row);
   }
 
 
